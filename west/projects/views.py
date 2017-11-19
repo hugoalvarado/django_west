97d2db76f1a6830e91276335e399c216d93c2bc6
Hugo Alvarado <hugo@britecore.com>
2017-11-19 10:24:14 -0600

    Add test crispy form and created_at updated_at columns to Project model

--- west/projects/views.py
+++ west/projects/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . forms import MessageForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'projects/index.html', {'form': MessageForm()})
 
-# Create your views here.
